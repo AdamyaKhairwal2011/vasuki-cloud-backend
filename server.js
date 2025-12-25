86cc6587c85157dc02cc9bc6077bcd9096f06531
Vasuki iTech <anuradhamaheshwari48@gmail.com>
2025-12-25 22:01:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const path = require("path");
 const fs = require("fs");
 const cors = require("cors");
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+const app = express();
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.options("*", cors());
+
 app.use(express.json());
+
 
 // Base uploads folder
 const UPLOADS_ROOT = path.join(__dirname, "uploads");
