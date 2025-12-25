432f3065b4a1cbe5d9ec3d38bc9512315c66f291
Vasuki iTech <anuradhamaheshwari48@gmail.com>
2025-12-25 22:09:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,23 +22,6 @@ app.use((req, res, next) => {
 /* ───────────────────────────────────────
    ABSOLUTE CORS FIX (NO DEPENDENCY)
    ─────────────────────────────────────── */
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204); // PRE-FLIGHT TERMINATION
-  }
-
-  next();
-});
 
 app.use(express.json());
 
@@ -134,4 +117,5 @@ app.post("/rename-file", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
